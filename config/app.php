e4fe25d720d70d9d1ea1a4f72e9db9221548b1a3
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-08-24 11:54:41 +0630

    add asset route-3

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://test-blog-flax.vercel.app'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', 'https://test-blog-flax.vercel.app'),
 
     /*
     |--------------------------------------------------------------------------
